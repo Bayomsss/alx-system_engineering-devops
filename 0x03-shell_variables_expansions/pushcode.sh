cc3d61c740918eaee6f0ed7d18d26f0fbb1df254
Bayomsss <57507129+Bayomsss@users.noreply.github.com>
2022-12-07 16:56:00 +0100

    'updated pushcode.sh'

--- 0x03-shell_variables_expansions/pushcode.sh
+++ 0x03-shell_variables_expansions/pushcode.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git add .
 
-#read -p "Enter commit message: " commit
+read -p "Enter commit message (include single quotes): " commit
 
-git commit -m  'commit'
+git commit -m "$commit"
 
 git push
